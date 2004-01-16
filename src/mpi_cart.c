621da96c2a49087a1656aea4428a0172f4ccb91a
Ralf Wildenhues <wildenhues@ins.uni-bonn.de>
2004-01-16 10:20:55 +0000

    Fix UMR in MPI_Cart_create: copy communicator value.

--- src/mpi_cart.c
+++ src/mpi_cart.c
@@ -7,6 +7,7 @@ int MPI_Cart_create(MPI_Comm comm_old, int ndims,
     int *dims, int *periods, int reorder, MPI_Comm *comm_cart)
 {
   NULLMPI_STATS;
+  *comm_cart = comm_old;
   return MPI_SUCCESS;
 }
 
